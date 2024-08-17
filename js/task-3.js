4dcfb075887b64aeaa2d9731976d056a6ae9e86c
yaraaos <yara144ss@gmail.com>
2024-08-17 23:39:21 +0300

    Done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,19 @@
 /* Перевірка спаму */
+
+function checkForSpam(message){
+    const lowerMessage = message.toLowerCase();
+
+    if (lowerMessage.includes('spam') || lowerMessage.includes('sale')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
